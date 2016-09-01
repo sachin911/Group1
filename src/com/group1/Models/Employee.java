86230cb9ea8d13cad750c23971953ae1d109ca1c
kbarker7 <kbarker00@gmail.com>
2016-09-01 14:17:41 +0530

    added variable fields to emp

--- src/com/group1/Models/Employee.java
+++ src/com/group1/Models/Employee.java
@@ -3,4 +3,9 @@ package com.group1.Models;
 public abstract class Employee {
 	String userName;
 	String password;
+	String firstName;
+	String lastName;
+	String role;
+	int loginAttempts;
+	int employeeId;
 }
