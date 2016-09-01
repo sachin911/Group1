c1edbb040712e0bc017e610d14d67edb19149855
sachin911 <sachin.narasimhan@nyu.edu>
2016-09-01 01:32:35 -0700

    added MVC classes

---
+++ src/com/group1/Models/Trader.java
@@ -0,0 +1,17 @@
+package com.group1.Models;
+
+public class Trader extends User {
+
+	@Override
+	void createTrade() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	boolean changePassword(String passWord) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
