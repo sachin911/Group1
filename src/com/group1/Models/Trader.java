69df8e0382be30370ba300c2d3070edc4d903e27
sachin911 <sachin.narasimhan@nyu.edu>
2016-09-01 01:38:02 -0700

    added new classes

--- src/com/group1/Models/Trader.java
+++ src/com/group1/Models/Trader.java
@@ -2,16 +2,20 @@ package com.group1.Models;
 
 public class Trader extends User {
 
-	@Override
 	void createTrade() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	boolean changePassword(String passWord) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	@Override
+	void makeTrade() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
---
+++ src/com/group1/Models/User.java
@@ -0,0 +1,5 @@
+package com.group1.Models;
+
+public abstract class User extends Employee{
+	abstract void makeTrade();
+}
