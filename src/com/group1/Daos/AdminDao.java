c9c4bac740c9cd258bcf34b65a5d69ce068cc910
kbarker7 <kbarker00@gmail.com>
2016-09-01 18:23:36 +0530

    added services and dao

--- src/com/group1/Daos/AdminDao.java
+++ src/com/group1/Daos/AdminDao.java
@@ -1,5 +1,28 @@
 package com.group1.Daos;
 
+import java.sql.Connection;
+
 public class AdminDao {
+	Jdbc jdbc = new Jdbc();
+	
+	public boolean addEmployee(String username, String password, String first_Name, String last_Name, String role) {
+		Connection con = jdbc.getCon();
+		boolean result = false;
+		
+		
+		
+		return false;
+	}
+	
+	public void removeEmployee() {
+		Connection con = jdbc.getCon();
+		
+	}
+	
+	public void editEmployee() {
+		Connection con = jdbc.getCon();
+		
+	}
+	
 
 }
