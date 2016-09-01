0f798c25b0b59d79a723c06b76e9b9e95d3af082
sachin911 <sachin.narasimhan@nyu.edu>
2016-09-01 05:11:57 -0700

    MVC stuff

---
+++ src/com/group1/Controllers/PMController.java
@@ -0,0 +1,5 @@
+package com.group1.Controllers;
+
+public class PMController {
+
+}
--- src/com/group1/Models/Admin.java
+++ src/com/group1/Models/Admin.java
@@ -1,8 +1,20 @@
 package com.group1.Models;
 
-public class Admin extends Employee {
+public class Admin  {
 
-	public Admin(String userName, String password, String firstName, String lastName, String role) {
-		super(userName, password, firstName, lastName, role);
-	}
+	String userName;
+	String password;
+	String first_Name;
+	String last_Name;
+	String role;
+	int employeeId;
+	int loginAttempts;
+	
+	public Admin(String userName, String password, String first_Name, String last_Name, String role) {
+		this.userName = userName;
+		this.password = password;
+		this.first_Name = first_Name;
+		this.last_Name = last_Name;
+		this.role = role;
+	}	
 }
