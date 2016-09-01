eb6dd554b805c700a906d9f464181170befb2443
narrayeswanth <ynarra@sapient.com>
2016-08-31 23:31:19 -0700

    yeswanth
    hey

--- src/test.java
+++ src/test.java
@@ -5,13 +5,13 @@ public class test {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
 		System.out.println("hello1");
-<<<<<<< HEAD
+
 		System.out.println("HEY");
-=======
+
 		System.out.println("hello2");
 		System.out.println("hello3");
 		System.out.println("hello4");
->>>>>>> origin/master
+
 
 	}
 
