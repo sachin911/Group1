7d979d9c6328b9dc837daf3ee1f44451153b7f2f
NishchithHebbar <nishchithhebbar@gmail.com>
2016-09-01 12:11:31 +0530

    This works

--- src/test.java
+++ src/test.java
@@ -3,15 +3,8 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
-		System.out.println("hello1");
 
-		System.out.println("HEY");
-
-		System.out.println("hello2");
-		System.out.println("hello3");
-		System.out.println("hello4");
-		System.out.println("hello5");
+		System.out.println("This works");
 
 
 	}
