91abe276b3827b6723325205cf2ce2ddc24b00c1
kbarker7 <kbarker00@gmail.com>
2016-09-01 19:59:13 +0530

    admin functions

--- src/com/group1/Controllers/AdminController.java
+++ src/com/group1/Controllers/AdminController.java
@@ -1,5 +1,17 @@
 package com.group1.Controllers;
 
-public class AdminController {
+import com.group1.Models.Admin;
+import com.group1.Models.Employee;
+import com.group1.Models.PM;
+import com.group1.Models.Trader;
 
+public class AdminController {
+	Employee e;
+	
+	AdminController(String username, String password, String first_name, String last_name, String role, int pm_id) {	
+
+	}
+	
+	
+	
 }
--- src/com/group1/Services/AdminService.java
+++ src/com/group1/Services/AdminService.java
@@ -1,10 +1,20 @@
 package com.group1.Services;
 
 import com.group1.Daos.AdminDao;
+import com.group1.Models.Employee;
 
 public class AdminService implements AdminServiceInterface {
 	AdminDao adminDao = new AdminDao();
 	
-//	adminDao.addUser(user);
+	public boolean addEmployee(Employee user) {
+		return adminDao.addEmployee(user);
+	}
+	
+	public boolean removeEmployee(Employee user) {
+		return adminDao.removeEmployee(user);
+	}
 
+	public boolean editEmployee(Employee user) {
+		return adminDao.editEmployee(user);
+	}
 }
