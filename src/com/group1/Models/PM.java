76f8ad94d870a8125107e51a8cceef8f90d62ab8
kbarker7 <kbarker00@gmail.com>
2016-09-01 14:28:17 +0530

    added constructors

--- src/com/group1/Models/PM.java
+++ src/com/group1/Models/PM.java
@@ -1,5 +1,28 @@
 package com.group1.Models;
 
-public class PM {
+public class PM extends User {
+
+	public PM(String userName, String password, String firstName, String lastName, String role) {
+		super(userName, password, firstName, lastName, role);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	void makeTrade() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void generateReport() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void generateHistory() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/com/group1/Models/Trader.java
+++ src/com/group1/Models/Trader.java
@@ -2,18 +2,24 @@ package com.group1.Models;
 
 public class Trader extends User {
 
-	void createTrade() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	boolean changePassword(String passWord) {
-		// TODO Auto-generated method stub
-		return false;
+	Trader(String userName, String password, String firstName, String lastName, String role) {
+		super(userName, password, firstName, lastName, role);
 	}
 
 	@Override
 	void makeTrade() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void generateReport() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void generateHistory() {
 		// TODO Auto-generated method stub
 		
 	}
