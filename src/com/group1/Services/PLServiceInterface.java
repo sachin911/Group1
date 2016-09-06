d28ffa1b352db1ce34810486f1079852b3f85965
NishchithHebbar <nishchithhebbar@gmail.com>
2016-09-06 16:12:04 +0530

    PL complete without PL function call

--- src/com/group1/Services/PLServiceInterface.java
+++ src/com/group1/Services/PLServiceInterface.java
@@ -1,5 +1,13 @@
 package com.group1.Services;
 
+import java.util.List;
+
+import com.group1.Models.Employee;
+
 public interface PLServiceInterface {
 
+		public List PLCalc(Employee user);
+		public float CalculatePL(String side, int open_quantity, int executed_price, int current_price);
+	
+	
 }
