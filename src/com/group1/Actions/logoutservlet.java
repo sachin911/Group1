2b81f9e3acab88bfe37db187787ef211033eddf5
ValliMariappan <valli241994@gmail.com>
2016-09-07 22:12:04 +0530

    fuck

--- src/com/group1/Actions/logoutservlet.java
+++ src/com/group1/Actions/logoutservlet.java
@@ -8,22 +8,24 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class logoutservlet
+ * Servlet implementation class LogoutServlet
  */
-@WebServlet("/logoutservlet")
-public class Logoutservlet extends HttpServlet implements Filter {
+@WebServlet("/LogoutServlet")
+@WebFilter("*.jsp")
+public class LogoutServlet extends HttpServlet implements Filter {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Logoutservlet() {
+    public LogoutServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -32,41 +34,37 @@ public class Logoutservlet extends HttpServlet implements Filter {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		doFilter(request, response, null);
+	
+	doFilter(request, response, null);
+	
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
-	@Override
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-			throws IOException, ServletException {
-		HttpServletResponse response = (HttpServletResponse) res;
-        
-        //if(session.getAttribute("obj")!=null)
-               
-               ((HttpServletRequest) req).getSession().invalidate();
-              ((HttpServletResponse) res).sendRedirect(((HttpServletRequest) req).getContextPath() + "/login.jsp");
-
-        // response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
-       // response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
-        // response.setDateHeader("Expires", 0); // Proxies.
-
-        
-   
-        
-       //  ((HttpServletResponse) res).sendRedirect(((HttpServletRequest) req).getContextPath() + "/login.jsp");
-        
-       // chain.doFilter(req, res);
+	//protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
 		
-	}
+		
+		
+//
+	
+	 @Override
+	    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		
+		
+		HttpServletResponse response = (HttpServletResponse) res;
+		 
+			 
+			((HttpServletRequest) req).getSession().invalidate();
+	      
+	  
+	       
+	        ((HttpServletResponse) res).sendRedirect(((HttpServletRequest) req).getContextPath() + "/Login1.jsp");
+		
+	   
+	 }
+	
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
