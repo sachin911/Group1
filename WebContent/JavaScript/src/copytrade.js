ae8465a0ae0b0767bf1621af19dbe86f2fe50015
isabellewis <ilewis@sapient.com>
2016-09-08 10:15:40 +0530

    changes
    changes

--- WebContent/JavaScript/src/copytrade.js
+++ WebContent/JavaScript/src/copytrade.js
@@ -11,13 +11,22 @@ function addRow() {
     $('#tablebody').append(row);
 };
 
+
 $('#makeatradeinstructions__table tbody tr td input.checkbox').on('change', function (e) {
     if( $(this).is(':not(:checked)')){
     	return false;
     }
-     var row = $(this).closest('tr').clone();
-     $('#maketradetable > tbody:last-child').append(row);
-     $(this).closest('tr').remove();
+    var row = $(this).closest('tr').clone();
+    $('#maketradetable > tbody:last-child').append(row);
+	$(this).closest('tr').remove();
 });
 
 
+$('#maketradetable tbody tr td clone input.checkbox').on('change', function (d) {
+    console.log("true");
+    if($(this).is(':not(:checked)')){
+    	var row = $(this).closest('tr').clone();
+	    $('#makeatradeinstructions__table > tbody:last-child').append(row);
+		$(this).closest('tr').remove();
+    }
+});
